9d62078a8d0bb19a3fb7feb1fa0b28af59c1cfce
kkrrish859k <kkrrish859@gmail.com>
2025-11-28 15:00:32 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import pickle
 import string
+import re
 from nltk.corpus import stopwords
-import nltk
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
 
-
 def transform_text(text):
     text = text.lower()
-    text = nltk.word_tokenize(text)
+
+    # ---- FIXED TOKENIZER (NO NLTK DOWNLOAD NEEDED) ----
+    text = re.findall(r'\b\w+\b', text)
 
     y = []
     for i in text:
@@ -32,8 +33,9 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 st.title("Email/SMS Spam Classifier")
 
@@ -43,28 +45,15 @@ if st.button('Predict'):
 
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
+    
     # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
+    
     # 3. predict
     result = model.predict(vector_input)[0]
+    
     # 4. Display
     if result == 1:
         st.header("Spam")
     else:
-
         st.header("Not Spam")
-        import streamlit as st
-        import pickle
-        import nltk
-
-        # ---- Add these lines immediately after imports ----
-        nltk.download('punkt')
-        nltk.download('punkt_tab')
-        nltk.download('stopwords')
-        # ----------------------------------------------------
-
-        from nltk.corpus import stopwords
-        from nltk.tokenize import word_tokenize
-def transform_text(text):
-    text = text.lower()
-    text = word_tokenize(text)
