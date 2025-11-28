e8906905f82cfb3acfced90194a96acd7e8aa364
kkrrish859k <kkrrish859@gmail.com>
2025-11-28 15:02:59 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -2,38 +2,38 @@ import streamlit as st
 import pickle
 import string
 import re
-from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
 
+STOPWORDS = set([
+    "i","me","my","myself","we","our","ours","ourselves","you","your","yours",
+    "yourself","yourselves","he","him","his","himself","she","her","hers","herself",
+    "it","its","itself","they","them","their","theirs","themselves","what","which",
+    "who","whom","this","that","these","those","am","is","are","was","were","be",
+    "been","being","have","has","had","having","do","does","did","doing","a","an",
+    "the","and","but","if","or","because","as","until","while","of","at","by","for",
+    "with","about","against","between","into","through","during","before","after",
+    "above","below","to","from","up","down","in","out","on","off","over","under",
+    "again","further","then","once","here","there","when","where","why","how","all",
+    "any","both","each","few","more","most","other","some","such","no","nor","not",
+    "only","own","same","so","than","too","very","s","t","can","will","just","don",
+    "should","now"
+])
+
 def transform_text(text):
     text = text.lower()
-
-    # ---- FIXED TOKENIZER (NO NLTK DOWNLOAD NEEDED) ----
+    
+    # regex tokenizer
     text = re.findall(r'\b\w+\b', text)
 
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
-
+    # remove stopwords & punctuation + stemming
+    y = [ps.stem(i) for i in text if i.isalnum() and i not in STOPWORDS]
+    
     return " ".join(y)
 
 
+# Load your pickled vectorizer and model
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
@@ -42,17 +42,10 @@ st.title("Email/SMS Spam Classifier")
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
-    # 1. preprocess
     transformed_sms = transform_text(input_sms)
-    
-    # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
-    
-    # 3. predict
     result = model.predict(vector_input)[0]
     
-    # 4. Display
     if result == 1:
         st.header("Spam")
     else:
